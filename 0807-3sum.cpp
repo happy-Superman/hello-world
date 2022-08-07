c27cfdaa081128487fd7e38e556e635c89d20df7
Zhanghqq <110549323+Zhanghqq@users.noreply.github.com>
2022-08-08 07:08:43 +0800

    Add files via upload

---
+++ 0807-3sum.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+      int num1 =0;
+      int num2 =0;
+      int sum =0;
+      scanf("%d%d",&num1,&num2);
+     
+      sum = num1 + num2;
+      printf("相加计算结果=%d\n",sum);
+      return 0;
+}
---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+
+    short age = 20;
+    float weight = 95.6f;	
+	
+   printf("%d\n",age);
+   return 0;	
+	
+	
+}
